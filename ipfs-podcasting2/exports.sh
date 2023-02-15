bc2f35669292c444c02e9b4fb64659b20a261d86
Eric P <ericpp@gmail.com>
2023-02-15 06:47:24 +0000

    no tor

--- ipfs-podcasting2/exports.sh
+++ ipfs-podcasting2/exports.sh
@@ -1,5 +1,1 @@
 export APP_IPFS_PODCASTING_P2P_PORT="4001"
-
-local app_ipfs_podcasting_p2p_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-p2p/hostname"
-
-export APP_IPFS_PODCASTING_P2P_HIDDEN_SERVICE="$(cat "${app_ipfs_podcasting_p2p_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
